61f17bcd7104d2bbaacf7ecba4842ffebeeeb11d
2018079116 <wjdwlstjd968@hanyang.ac.kr>
2023-01-19 13:12:18 +0900

    champion, summoner API

--- data/views.py
+++ data/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from rest_framework.response import Response
+from .models import Summoner_rank, Champion
+from rest_framework.views import APIView
+from .serializer import SummonerSerializer, ChampionSerializer
+
+
+
+class ChampionAPI(APIView):
+    def get(self, request):
+        queryset = Champion.objects.all()
+        # print(queryset)
+        serializer = ChampionSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+class SummonerAPI(APIView):
+    def get(self, request):
+        queryset = Summoner_rank.objects.all()
+        print(queryset)
+        serializer = SummonerSerializer(queryset, many=True)
+        return Response(serializer.data)
--- gganbu/urls.py
+++ gganbu/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from data.views import SummonerAPI, ChampionAPI
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/summoner/', SummonerAPI.as_view()),
+    path('api/champion/', ChampionAPI.as_view()),
 ]
