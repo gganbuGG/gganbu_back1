b63b998f8ff528514ab47ecd8e39bec2b9e9803a
2018079116 <wjdwlstjd968@hanyang.ac.kr>
2023-01-27 15:15:15 +0900

    .

--- gganbu/settings.py
+++ gganbu/settings.py
@@ -38,9 +38,9 @@ def get_secret(setting):
 SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.pythonanywhere.com']
+ALLOWED_HOSTS = ['.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
