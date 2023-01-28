17bcb08f5d025d825a02a2922bd23eabc85de76f
2018079116 <wjdwlstjd968@hanyang.ac.kr>
2023-01-28 17:22:35 +0900

    emptybag

--- data/management/commands/testtest.py
+++ data/management/commands/testtest.py
@@ -1,7 +1,8 @@
 from data.models import Combinations, Combinations_partner, Champion
 from django.core.management.base import BaseCommand
+from collections import Counter
 
-def champData(name):
+def champData(name, how):
     champ = {
     "TFT8_Kaisa" : 
     {
@@ -13,141 +14,272 @@ def champData(name):
         "name" : "럭스",
         "traits" : ["주문투척자","별 수호자"]
     },
-    "TFT8_Kayle" : "케일",
-    "TFT8_Poppy" : "뽀삐",
-    "TFT8_Galio" : "갈리오",
-    "TFT8_Rell" : "렐",
-    "TFT8_Velkoz" : "벨코즈",
-    "TFT8_Nilah" : "닐라",
-    "TFT8_Ashe" : "애쉬",
-    "TFT8_Yasuo" : "야스오",
-    "TFT8_Senna" : "세나",
-    "TFT8_Zed" : "제드",
-    "TFT8_Lulu" : "룰루",
-    "TFT8_Nunu" : "누누",
-    "TFT8_Yuumi" : "유미",
-    "TFT8_Zoe" : "조이",
-    "TFT8_Taliyah" : "탈리야",
-    "TFT8_Sivir" : "시비르",
-    "TFT8_Gangplank" : "갱플랭크",
-    "TFT8_WuKong" : "오공",
-    "TFT8_Draven" : "드레이븐",
-    "TFT8_Malphite" : "말파이트",
-    "TFT8_Nasus" : "나서스",
-    "TFT8_Jinx" : "징크스",
-    "TFT8_Vayne" : "베인",
-    "TFT8_MissFortune" : "미스포츈",
-    "TFT8_Chogath" : "초가스",
-    "TFT8_Rammus" : "람머스",
-    "TFT8_BelVeth" : "벨베스",
-    "TFT8_AurelionSol" : "아우렐리온솔",
-    "TFT8_Zac" : "자크",
-    "TFT8_Mordekaiser" : "모데카이저",
-    "TFT8_Leblanc" : "르블랑",
-    "TFT8_Sylas" : "사일러스",
-    "TFT8_Camille" : "카밀",
-    "TFT8_Ezreal" : "이즈리얼",
-    "TFT8_Sona" : "소나",
-    "TFT8_Ekko" : "에코",
-    "TFT8_Sett" : "세트",
-    "TFT8_Janna" : "잔나",
-    "TFT8_Urgot" : "우르곳",
-    "TFT8_Syndra" : "신드라",
-    "TFT8_Fiddlesticks" : "피들스틱",
-    "TFT8_Blitzcrank" : "블리츠크랭크",
-    "TFT8_Renekton" : "레넥톤",
-    "TFT8_Vi" : "바이",
-    "TFT8_LeeSin" : "리신",
-    "TFT8_Riven" : "리븐",
-    "TFT8_Jax" : "잭스",
-    "TFT8_Sejuani" : "세주아니",
-    "TFT8_Soraka" : "소라카",
-    "TFT8_Talon" : "탈론",
-    "TFT8_Fiora" : "피오라",
-    "TFT8_Annie" : "애니",
-    "TFT8_Alistar" : "알리스타",
-    "TFT8_Viego" : "비에고",
-    "TFT8_Samira" : "사미라",
-    "TFT8_Aphelios" : "아펠리오스",
-    "TFT8_Leona" : "레오나"
+    "TFT8_Kayle" : {
+        "name" : "케일",
+        "traits" : ["지하세계", "결투가"]
+    },
+    "TFT8_Poppy" : {
+        "name" : "뽀삐",
+        "traits" : ["기계유망주","엄호대"]
+    },
+    "TFT8_Galio" : {
+        "name" : "갈리오",
+        "traits" : ["민간인", "마스코트"]
+    },
+    "TFT8_Rell" : {
+        "name" : "렐",
+        "traits" : ["별 수호자","엄호대"]
+    },
+    "TFT8_Velkoz" : {
+        "name" : "벨코즈",
+        "traits" : ["위협"]
+    },
+    "TFT8_Nilah" : {
+        "name": "닐라",
+        "traits" : ["별 수호자", "결투가"]
+    },
+    "TFT8_Ashe" : {
+        "name":"애쉬",
+        "traits" : ["정찰단", "레이저단"]
+    },
+    "TFT8_Yasuo" : {
+        "name":"야스오",
+        "traits" : ["레이저단", "결투가"]
+    },
+    "TFT8_Senna" : {
+        "name":"세나",
+        "traits" : ["레이저단", "특등사수"]
+    },
+    "TFT8_Zed" : {
+        "name":"제드",
+        "traits" : ["결투가", "레이저단", "해커"]
+    },
+    "TFT8_Lulu" : {
+        "name":"룰루",
+        "traits" : ["선의", "기계유망주"]
+    },
+    "TFT8_Nunu" : {
+        "name":"누누",
+        "traits" : ["마스코트", "기계유망주"]
+    },
+    "TFT8_Yuumi" : {
+        "name":"유미",
+        "traits" : ["별 수호자", "선의","마스코트"]
+    },
+    "TFT8_Zoe" : {
+        "name":"조이",
+        "traits" : ["기계유망주","익살꾼","해커"]
+    },
+    "TFT8_Taliyah" : {
+        "name":"탈리야",
+        "traits" : ["별 수호자", "주문투척자"]
+    },
+    "TFT8_Sivir" : {
+        "name":"시비르",
+        "traits" : ["민간인", "특등사수"]
+    },
+    "TFT8_Gangplank" : {
+        "name":"갱플랭크",
+        "traits" : ["결투가","우세"]
+    },
+    "TFT8_WuKong" : {
+        "name":"오공",  
+        "traits" : ["메카: 프라임", "엄호대"]
+    },
+    "TFT8_Draven" : {
+        "name":"드레이븐",
+        "traits" : ["메카: 프라임", "에이스"]
+    },
+    "TFT8_Malphite" : {
+        "name":"말파이트",
+        "traits" : ["우세","마스코트"]
+    },
+    "TFT8_Nasus" : {
+        "name":"나서스",
+        "traits" : ["마스코트","동물특공대"]
+    },
+    "TFT8_Jinx" : {
+        "name":"징크스",
+        "traits" : ["동물특공대","익살꾼"]
+    },
+    "TFT8_Vayne" : {
+        "name":"베인",
+        "traits" : ["동물특공대","정찰단","결투가"]
+    },
+    "TFT8_MissFortune" : {
+        "name":"미스포츈",
+        "traits" : ["에이스","동물특공대"]
+    },
+    "TFT8_Chogath" : {
+        "name":"초가스",
+        "traits" : ["위협"]
+    },
+    "TFT8_Rammus" : {
+        "name":"람머스",
+        "traits" : ["위협"]
+    },
+    "TFT8_BelVeth" : {
+        "name":"벨베스",
+        "traits" : ["위협"]
+    },
+    "TFT8_AurelionSol" : {
+        "name":"아우렐리온솔",
+        "traits" : ["위협"]
+    },
+    "TFT8_Zac" : {
+        "name":"자크",
+        "traits" : ["위협"]
+    },
+    "TFT8_Mordekaiser" : {
+        "name":"모데카이저",
+        "traits" : ["에이스", "레이저단"]
+    },
+    "TFT8_Leblanc" : {
+        "name":"르블랑",
+        "traits" : ["자동방어체계", "해커","주문투척자"]
+    },
+    "TFT8_Sylas" : {
+        "name":"사일러스",
+        "traits" : ["동물특공대", "무법자"]
+    },
+    "TFT8_Camille" : {
+        "name":"카밀",
+        "traits" : ["자동방어체계","무법자"]
+    },
+    "TFT8_Ezreal" : {
+        "name":"이즈리얼",
+        "traits" : ["지하세계","정찰단"]
+    },
+    "TFT8_Sona" : {
+        "name":"소나",
+        "traits" : ["지하세계", "선의", "주문투척자"]
+    },
+    "TFT8_Ekko" : {
+        "name":"에코",
+        "traits" : ["별 수호자", "방패대","익살꾼"]
+    },
+    "TFT8_Sett" : {
+        "name":"세트",
+        "traits" : ["메카: 프라임", "엄호대"]
+    },
+    "TFT8_Janna" : {
+        "name":"잔나",
+        "traits" : ["민간인","기상캐스터","주문투척자"]
+    },
+    "TFT8_Urgot" : {
+        "name":"우르곳",
+        "traits" : ["위협"]
+    },
+    "TFT8_Syndra" : {
+        "name":"신드라",
+        "traits" : ["선의","별 수호자"]
+    },
+    "TFT8_Fiddlesticks" : {
+        "name":"피들스틱",
+        "traits" : ["위협","타락"]
+    },
+    "TFT8_Blitzcrank" : {
+        "name":"블리츠크랭크",
+        "traits" : ["자동방어체계", "싸움꾼"]
+    },
+    "TFT8_Renekton" : {
+        "name":"레넥톤",
+        "traits" : ["싸움꾼","레이저단"]
+    },
+    "TFT8_Vi" : {
+        "name":"바이",
+        "traits" : ["싸움꾼", "방패대","지하세계"]
+    },
+    "TFT8_LeeSin" : {
+        "name":"리신",
+        "traits" : ["선의","싸움꾼"]
+    },
+    "TFT8_Riven" : {
+        "name":"리븐",
+        "traits" : ["동물특공대", "싸움꾼", "엄호대"]
+    },
+    "TFT8_Jax" : {
+        "name":"잭스",
+        "traits" : ["메카: 프라임", "싸움꾼"]
+    },
+    "TFT8_Sejuani" : {
+        "name":"세주아니",
+        "traits" : ["싸움꾼", "레이저단"]
+    },
+    "TFT8_Soraka" : {
+        "name":"소라카",
+        "traits" : ["자동방어체계", "선의"]
+    },
+    "TFT8_Talon" : {
+        "name":"탈론",
+        "traits" : ["황소부대", "무법자"]
+    },
+    "TFT8_Fiora" : {
+        "name":"피오라",
+        "traits" : ["황소부대", "결투가"]
+    },
+    "TFT8_Annie" : {
+        "name":"애니",
+        "traits" : ["황소부대", "기계유망주", "주문투척자"]
+    },
+    "TFT8_Alistar" : {
+        "name":"알리스타",
+        "traits" : ["황소부대", "마스코트", "방패대"]
+    },
+    "TFT8_Viego" : {
+        "name":"비에고",
+        "traits" : ["황소부대", "무법자"]
+    },
+    "TFT8_Samira" : {
+        "name":"사미라",
+        "traits" : ["에이스", "지하세계","특등사수"]
+    },
+    "TFT8_Aphelios" : {
+        "name":"아펠리오스",
+        "traits" : ["황소부대", "특등사수", "병기고"]
+    },
+    "TFT8_Leona" : {
+        "name":"레오나",
+        "traits" : ["메카: 프라임", "무법자", "방패대"]
+    },
     }
-
-    return champ[name]
-
-def EtoKItem(name):
-    item = {
-        "TFT_Item_GargoyleStoneplate" : "가고일 돌갑옷",
-        "TFT_Item_IonicSpark" : "이온충격기",
-        "TFT_Item_ZekesHerald" : "지크의 전령",
-        "TFT_Item_ArchangelsStaff" : "대천사의 지팡이",
-        "TFT8_Item_HeartEmblemItem" : "선의 상징",
-        "TFT_Item_JeweledGauntlet" : "보석 건틀릿",
-        "TFT_Item_MadredsBloodrazor" : "거인학살자",
-        "TFT_Item_StatikkShiv" : "스태틱의 단검",
-        "TFT_Item_DragonsClaw" : "용의 발톱",
-        "TFT_Item_RedBuff" : "태양불꽃 망토",
-        "TFT_Item_UnstableConcoction" : "정의의 손길",
-        "TFT_Item_LastWhisper" : "최후의 속삭임",
-        "TFT_Item_SpearOfShojin" : "쇼진의 창",
-        "TFT_Item_TitanicHydra" : "즈롯 차원문",
-        "TFT_Item_GuardianAngel" : "밤의 끝자락",
-        "TFT_Item_ThiefsGloves" : "도적의 장갑",
-        "TFT_Item_PowerGauntlet" : "방패파괴자",
-        "TFT_Item_FrozenHeart" : "수호자의 맹세",
-        "TFT_Item_LocketOfTheIronSolari" : "강철의 솔라리 펜던트",
-        "TFT_Item_Redemption" : "구원",
-        "TFT_Item_RunaansHurricane" : "루난의 허리케인",
-        "TFT8_Item_DuelistEmblemItem" : "결투가 상징",
-        "TFT_Item_SeraphsEmbrace" : "푸른 파수꾼",
-        "TFT_Item_HextechGunblade" : "마법공학 총검",
-        "TFT_Item_Zephyr" : "서풍",
-        "TFT8_Item_AnimaSquadEmblemItem" : "동물특공대 상징",
-        "TFT_Item_BrambleVest" : "덤불조끼",
-        "TFT8_Item_InterPolarisEmblemItem" : "레이저단 상징",
-        "TFT8_Item_OxForceEmblemItem" : "황소부대 상징",
-        "TFT_Item_GuinsoosRageblade" : "구인수의 격노검",
-        "TFT_Item_InfinityEdge" : "무한의 대검",
-        "TFT_Item_TitansResolve" : "거인의 결의",
-        "TFT8_Item_DefenderEmblemItem" : "엄호대 상징",
-        "TFT_Item_Bloodthirster" : "피바라기",
-        "TFT4_Item_OrnnEternalWinter" : "영원한 겨울",
-        "TFT_Item_WarmogsArmor" : "워모그의 갑옷",
-        "TFT_Item_RabadonsDeathcap" : "라바돈의 죽음모자",
-        "TFT_Item_Morellonomicon" : "모렐로노미콘",
-        "TFT8_Item_RenegadeEmblemItem" : "무법자 상징",
-        "TFT8_Item_AceEmblemItem" : "에이스 상징",
-        "TFT8_Item_BrawlerEmblemItem" : "싸움꾼 상징",
-        "TFT_Item_Chalice" : "힘의 성배",
-        "TFT8_Item_CivilianEmblemItem" : "민간인 상징",
-        "TFT4_Item_OrnnAnimaVisage" : "영혼의 형상",
-        "TFT8_Item_ExoPrimeEmblemItem" : "메카: 프라임 상징",
-        "TFT4_Item_OrnnMuramana" : "마나자네",
-        "TFT_Item_Quicksilver" : "수은",
-        "TFT_Item_RapidFireCannon" : "고속 연사포",
-        "TFT_Item_Shroud" : "침묵의 장막",
-        "TFT_Item_Deathblade" : "죽음의 검",
-        "TFT8_Item_MascotEmblemItem" : "마스코트 상징",
-        "TFT8_Item_DeadeyeEmblemItem" : "특등사수 상징",
-        "TFT4_Item_OrnnRocketPropelledFist" : "로켓 주먹",
-        "TFT8_Item_AegisEmblemItem" : "방패대 상징",
-        "TFT8_Item_ReconEmblemItem" : "정찰단 상징",
-        "TFT_Item_ForceOfNature" : "전략가의 왕관",
-        "TFT8_Item_ADMINEmblemItem" : "자동방어체계 상징",
-        "TFT4_Item_OrnnZhonyasParadox" : "존야의 역설",
-        "TFT4_Item_OrnnInfinityForce" : "무한한 삼위일체",
-        "TFT4_Item_OrnnDeathsDefiance" : "죽음의 저항",
-        "TFT4_Item_OrnnTheCollector" : "황금 징수의 총",
-        "TFT4_Item_OrnnObsidianCleaver" : "흑요석 양날 도끼",
-    }
-    return item[name]
+    if how == "name":
+        return champ[name]["name"]
+    elif how == "traits":
+        return champ[name]["traits"]
 
 class Command(BaseCommand):
     
     def handle(self, *args, **kwargs):
-        cs = Champion.objects.all()
-        for c in cs:
-            temp = []
-            items = c.items
-            for item in items:
-                temp.append(EtoKItem(item))
-            print(temp)
+        
+        """
+        콤비네이션오브젝트 하나하나마다
+        오브젝트하나의 id로 파트너 오브젝트 찾고
+        콤비네이션 유닛 배열 - 파트너 유닛배열 -> 이게 하나의 객체
+        조건문으로 콤비네이션 유닛배열이 콤비네이ㅕㄴ 유닛배열 이나 파트너 유닛배열 이랑 같고
+        파트너 배열이 나머지 하나랑 같으면 count + 1
+        아니면 새로운 객체 생성
+        이런식으로 쭉쭉 만들어서
+        통계 얻기
+
+        """
+
+        deck1s = Combinations.objects.all()
+        decks = []
+        decksString = []
+        for deck1 in deck1s:
+            deck2 = Combinations_partner.objects.filter(id = deck1.id)
+            if not deck2:
+                print("id 같은게 없습니다")
+                return
+            deck1_units = []
+            deck2_units = []
+            for unit in deck1.units:
+                deck1_units.append(champData(unit["character_id"],"name"))
+            for unit in deck2[0].units:
+                deck2_units.append(champData(unit["character_id"],"name"))
+            temp = [deck1_units, deck2_units]
+            temp.sort()
+            decks.append(temp)
+            decksString.append(str(temp))
+        #데이터 더 들어와야 의미있는 결과 얻을 듯
