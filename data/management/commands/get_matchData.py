6c40acd2db0dfba2164f19713c0b23cae61f614b
2018079116 <wjdwlstjd968@naver.com>
2023-03-13 14:19:27 +0900

    버전정보

--- data/management/commands/get_matchData.py
+++ data/management/commands/get_matchData.py
@@ -98,7 +98,7 @@ def get_matchData(puuids, API_KEY):
         for matchid in ids:
             data = askRiot(f'https://asia.api.riotgames.com/tft/match/v1/matches/{matchid}?api_key={API_KEY}')
             version = data['info']['game_version']
-            if version[:12] != "Version 13.5":
+            if version[:12] != "Version 13.5" and version[:12] != "Version 13.1":
                 print("롤토체스 데이터 업데이트")
                 break
             #더블업 모드만 저장
